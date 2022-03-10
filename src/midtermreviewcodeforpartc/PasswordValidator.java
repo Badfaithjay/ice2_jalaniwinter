47c3f097bd5babbf9a366927c7265fa0c1e6674d
Badfaithjay <maynajal@sheridancollege.ca>
2022-03-09 20:27:15 -0500

    added passwordvalidator

---
+++ src/midtermreviewcodeforpartc/PasswordValidator.java
@@ -0,0 +1,39 @@
+
+package midtermreviewcodeforpartc;
+
+import java.util.Scanner;
+
+
+public class PasswordValidator 
+{
+            String password="";
+
+      public void check()
+      {
+                Scanner sc = new Scanner(System.in);
+
+        boolean validPassword=false;
+        while(!validPassword)
+        {
+            System.out.println("Passwords must have at least 8 characters");
+            System.out.println("Passwords must have at least one special character");
+            System.out.println("Please enter your desired password:"); 
+            
+            password = sc.nextLine();
+            int specialCharCount=0;
+            //iterate over each character to see if it is a special character
+            for(int i=0;i<password.length(); i++)
+            {
+                if(!(Character.isLetterOrDigit(password.charAt(i))))
+                {
+                    //now we know there is at least one special character
+                    specialCharCount++;
+                }
+            }
+            if(specialCharCount>0 &&password.length()>7)
+            {
+                validPassword=true;
+            }
+        }
+      }
+}
